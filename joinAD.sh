2bf135f6ca8f1d58645756745564951fbeb0f020
SaRoCS <samuel.schroeder13@gmail.com>
2023-04-15 14:37:28 -0500

    join script

---
+++ joinAD.sh
@@ -0,0 +1,28 @@
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    KEY_LENGTH=${#KEY}
+    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+    export "$KEY"="$VALUE"
+done
+
+hostnamectl set-hostname $box_name.team$team_num.isucdc.com
+apt update
+apt upgrade -y
+apt install -y realmd ntp
+sed -i "/pool 0.ubuntu.pool.ntp.org iburst/i server ad.team$team_num.isucdc.com" /etc/ntp.conf
+realm discover team$team_num.isucdc.com
+apt install -y krb5-user
+# enter TEAM5.ISUCDC.COM
+kinit administrator
+realm join team$team_num.isucdc.com
+sed -i 's/use_fully_qualified_names = True/use_fully_qualified_names = False/' /etc/sssd/sssd.conf
+service sssd --full-restart
+sed -i "/pam_unix.so/a session required\tpam_mkhomedir.so skel=/etc/skel/ umask=0022" /etc/pam.d/common-session
+sed -i "/%admin ALL=(ALL) ALL/a \\n# Allow Domain Admins to use sudo\n%Domain\ Admins@team$team_num.isucdc.com ALL=(ALL) NOPASSWD:ALL\n%Administrators@team$team_num.isucdc.com ALL=(ALL) NOPASSWD:ALL" /etc/sudoers
+# realm permit -g group@domain
+realm permit --all
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service ssh restart
+reboot
