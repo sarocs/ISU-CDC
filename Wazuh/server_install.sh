a9cda9b26bdd1400c8460e2bd833018a87014c6f
SaRoCS <samuel.schroeder13@gmail.com>
2024-02-10 20:05:02 +0000

    Added ability to enable active response

--- Wazuh/server_install.sh
+++ Wazuh/server_install.sh
@@ -1,3 +1,5 @@
+read -p "Do you want to enable Active Response (y/n): " ar
+
 curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh && bash ./wazuh-install.sh -a
 
 cp ../ISU-CDC-Private/Wazuh/agent.conf /var/ossec/etc/shared/default
@@ -14,5 +16,15 @@ sed -i '/vulnerability-detector/!b;n; s/<enabled>no/<enabled>yes/' /var/ossec/et
 # Enable Ubuntu vulnerabilites
 # Other options: debian, redhat, alas, suse, arch, almalinux
 sed -i '/<provider name="canonical">/!b;n; s/<enabled>no/<enabled>yes/' /var/ossec/etc/ossec.conf
+
+# Move active response configuration
+ar_conf=$(cat ../ISU-CDC-Private/Wazuh/ar.conf | sed -z 's/\r\n/\\n/g')
+sed -i "s~    active-response options here~$ar_conf~" /var/ossec/etc/ossec.conf
+# Enable active response
+if [ "$ar" != 'n' ]
+then
+    sed -i '/<!--/ { h; N; /<active-response>/ D; }' /var/ossec/etc/ossec.conf
+    sed -i '/<\/active-response>/ { h; n; g; d; }' /var/ossec/etc/ossec.conf
+fi
 
 systemctl restart wazuh-manager
