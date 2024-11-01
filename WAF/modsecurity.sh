060aa5506cfb6c635b117164b47a50d09f99c407
SaRoCS <samuel.schroeder13@gmail.com>
2024-11-01 17:43:28 -0500

    Update ModSecurity script for Ubuntu 24

--- WAF/modsecurity.sh
+++ WAF/modsecurity.sh
@@ -2,52 +2,54 @@
 
 read -p "Cores: " cores
 read -p "Domain: " domain
-read -p "Install Directory: " dir
-sudo apt update 
-sudo apt install -y libtool autoconf build-essential libpcre3-dev zlib1g-dev libssl-dev libxml2-dev libgeoip-dev liblmdb-dev libyajl-dev libcurl4-openssl-dev libpcre++-dev pkgconf libxslt1-dev libgd-dev automake
-sudo mkdir -p $dir
+dir='/usr/local/modsecurity'
+
+apt update 
+apt install -y git g++ gcc libtool autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre3-dev libssl-dev libxml2-dev libyajl-dev pkgconf zlib1g-dev
+mkdir -p $dir
 cd $dir
 
 # Build ModSecurity
-git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity
+git clone https://github.com/owasp-modsecurity/ModSecurity
 cd ModSecurity
 git submodule init
 git submodule update
 ./build.sh
 ./configure
 make -j $cores
-sudo make install
+make install
 cd ..
 
 # Build Nginx Connector
-git clone https://github.com/SpiderLabs/ModSecurity-nginx.git
+git clone https://github.com/owasp-modsecurity/ModSecurity-nginx.git
 nginx_version=$(nginx -v 2>&1 | grep -o '[0-9]\.[0-9]*\.[0-9]*')
 wget http://nginx.org/download/nginx-$nginx_version.tar.gz
 tar xzf nginx-$nginx_version.tar.gz
 cd nginx-$nginx_version
 ./configure --with-compat --with-openssl=/usr/include/openssl/ --add-dynamic-module=$dir/ModSecurity-nginx
 make modules
-sudo cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/
-sudo chmod 644 /usr/share/nginx/modules/ngx_http_modsecurity_module.so
+mkdir -p /usr/lib/nginx/modules
+cp objs/ngx_http_modsecurity_module.so /usr/lib/nginx/modules/
+chmod 644 /usr/share/nginx/modules/ngx_http_modsecurity_module.so
 cd ..
-sudo sed -i '/include \/etc\/nginx\/modules-enabled\/\*.conf/a\load_module modules/ngx_http_modsecurity_module.so;' /etc/nginx/nginx.conf
+sed -i '/include \/etc\/nginx\/modules-enabled\/\*.conf/a\load_module modules/ngx_http_modsecurity_module.so;' /etc/nginx/nginx.conf
 
 # Configure ModSecurity
-sudo mkdir /etc/nginx/modsec
-sudo wget -P /etc/nginx/modsec/ https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
-sudo mv /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf
-sudo cp ModSecurity/unicode.mapping /etc/nginx/modsec
-sudo sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf
-sudo sed -i '/location/i\\tmodsecurity on;\n\tmodsecurity_rules_file /etc/nginx/modsec/main.conf;' /etc/nginx/sites-enabled/$domain
+mkdir /etc/nginx/modsec
+wget -P /etc/nginx/modsec/ https://raw.githubusercontent.com/owasp-modsecurity/ModSecurity/v3/master/modsecurity.conf-recommended
+mv /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf
+cp ModSecurity/unicode.mapping /etc/nginx/modsec
+sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf
+sed -i '/location/i\\tmodsecurity on;\n\tmodsecurity_rules_file /etc/nginx/modsec/main.conf;' /etc/nginx/sites-enabled/$domain
 
 # Enable OWASP Rules
 git clone https://github.com/coreruleset/coreruleset.git
 cp coreruleset/crs-setup.conf.example coreruleset/crs-setup.conf
-sudo bash -c 'echo "Include /etc/nginx/modsec/modsecurity.conf
+bash -c 'echo "Include /etc/nginx/modsec/modsecurity.conf
 Include $(pwd)/coreruleset/crs-setup.conf
 Include $(pwd)/coreruleset/rules/*.conf" > /etc/nginx/modsec/main.conf'
 
 # Change Security Action to Redirect
 echo -e "SecRuleUpdateActionById 949110 \"t:none,redirect:'%{REQUEST_FILENAME}'\"\nSecRuleUpdateActionById 959100 \"t:none,redirect:'%{REQUEST_FILENAME}'\"" > coreruleset/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
 
-sudo nginx -s reload
+nginx -s reload
