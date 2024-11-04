68675b1067a84994133c9416951604881c9cdfb0
SaRoCS <samuel.schroeder13@gmail.com>
2024-11-04 09:58:39 -0600

    Support proxy for multiple hosts

--- WAF/proxy.sh
+++ WAF/proxy.sh
@@ -5,10 +5,37 @@ apt install nginx -y
 
 ufw allow 'Nginx HTTP'
 
-read -p "Domain: " domain
-read -p "Webserver IP: " web_ip
+temp="server {
+        listen {{port}};
 
-echo -e "server {\n\tlisten 80;\n\tlisten [::]:80;\n\n\t# For personal testing can use _, may need modifications for competition environment\n\tserver_name $domain www.$domain;\n\n\tlocation / {\n\t\tproxy_pass http://$web_ip;\n\t\tinclude proxy_params;\n\t}\n}" > /etc/nginx/sites-available/$domain
+        server_name {{host}}.{{domain}};
+
+        location / {
+                proxy_pass http://{{ip}}:{{port}};
+                include proxy_params;
+        }
+}"
+
+read -p "Domain: " domain
+
+while true
+do
+        read -p "Host: " host
+        read -p "IP: " ip
+        read -p "Port: " port
+
+        block="${temp//\{\{port\}\}/$port}"
+        block="${block//\{\{host\}\}/$host}"
+        block="${block//\{\{domain\}\}/$domain}"
+        block="${block//\{\{ip\}\}/$ip}"
+
+        echo -e "$block" >> /etc/nginx/sites-available/$domain
+
+        read -p "Do you want to continue? (y/n): " continue
+        if [[ "$continue" != "y" ]]; then
+                break
+        fi
+done
 
 ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
